a526742d2f8067e56f945e90d5c62f746ea713f3
Xtreme512 <atikba@gmail.com>
2015-12-05 12:34:31 +0100

    testtttttt

--- ASPM Hospital Management/src/Client.java
+++ ASPM Hospital Management/src/Client.java
@@ -4,7 +4,7 @@ public class Client {
 	public static void main(String[] args) {
 	PatientInfo frame = new PatientInfo();
 		frame.setVisible(true);
-		System.out.println("6789");
+		System.out.println("67dfgdfg89");
 		//Doctors_info frame = new Doctors_info();
 		//frame.setVisible(true);
 
